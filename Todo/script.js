8206bb1a30199a18920af51f15474386b39a4153
Wraith <i.acnaylor@gmail.com>
2021-02-08 13:23:12 +0000

    Started Creating a Todo List app

---
+++ Todo/script.js
@@ -0,0 +1,22 @@
+let idx = 0;
+
+function item() {
+    let item = document.createElement("div"); // create div element
+    item.setAttribute("class", "text-input"); // give it a class
+    item.setAttribute("id", `div${idx}`); // give it a unique id
+
+    let inp = document.createElement("input"); // create input element
+    inp.setAttribute("type", "text"); // give it a type
+    inp.setAttribute("class", "inp"); // give it a class
+    inp.setAttribute("id", `inp${idx}`) // give it a unique id
+    inp.setAttribute("placeholder", "Todo - Item"); // give it a placeholder
+    inp.setAttribute("onchange", "replace()"); // give it an onchange function
+    item.appendChild(inp); // appending that element onto the div
+
+    document.querySelector(".cont").appendChild(item); // appending the div element onto the main div of the website
+    idx++; // incrementing the idx to ensure unique IDs
+};
+
+function replace() {
+    console.log("replace mfs be like changed noises")
+};
