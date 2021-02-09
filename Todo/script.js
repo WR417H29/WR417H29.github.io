24abf6aa9eb96097cb0e83bf994f3f832ff838a7
Wraith <i.acnaylor@gmail.com>
2021-02-09 00:20:36 +0000

    Made more finalisations to Todo app

--- Todo/script.js
+++ Todo/script.js
@@ -1,6 +1,10 @@
 let idx = 0;
+let data = {};
+
 
 function item() {
+    data[`item${idx}`] = [{"index": idx, "editable": true, "fin": false}];
+
     let item = document.createElement("div"); // create div element
     item.setAttribute("class", "text-input"); // give it a class
     item.setAttribute("id", `div${idx}`); // give it a unique id
@@ -10,36 +14,46 @@ function item() {
     inp.setAttribute("class", "inp"); // give it a class
     inp.setAttribute("id", `inp${idx}`) // give it a unique id
     inp.setAttribute("placeholder", "Todo - Item"); // give it a placeholder
-    inp.setAttribute("onchange", `replace(inp${idx})`); // give it an onchange function
+    inp.setAttribute("onchange", `update(${idx})`); // give it an onchange function
+    inp.style.display = "block";
+    
+    let radioSwap = document.createElement("input");
+    radioSwap.setAttribute("type", "radio");
+    radioSwap.setAttribute("id", `rs${idx}`);
+    radioSwap.setAttribute("onClick", `update(${idx})`)
+    radioSwap.style.display = "none";
+
+    let textDisp = document.createElement("p");
+    textDisp.setAttribute("id", `p${idx}`);
+    textDisp.style.display = "none";
+
     item.appendChild(inp); // appending that element onto the div
+    item.appendChild(radioSwap);
+    item.appendChild(textDisp);
 
     document.querySelector(".cont").appendChild(item); // appending the div element onto the main div of the website
+    update(idx);
     idx++; // incrementing the idx to ensure unique IDs
 };
 
-function replace(elemID) {
-    let par = elemID.parentElement;
-    let val = elemID.value;
-    let newElem = document.createElement("p");
-    newElem.setAttribute("id", `p${idx}`);
+function update(index) {
+    let inputElem = document.getElementById(`inp${index}`);
+    let outputElem = document.getElementById(`p${index}`);
+    let radioElem = document.getElementById(`rs${index}`);
 
-    const checkbox = document.createElement("input");
-    checkbox.setAttribute("id", `cb${idx}`);
-    checkbox.setAttribute("type", "radio");
-    checkbox.setAttribute("onClick", `reset(${elemID})`);
+    if (data[`item${index}`]['editable'] == true) {
+        data[`item${index}`]['editable'] = false;
+        inputElem.style.display = "none";
+        outputElem.style.display = "block";
+        radioElem.style.display = "block";
 
-    newElem.innerText = val;
-    par.removeChild(elemID);
-    par.appendChild(checkbox);
-    par.appendChild(newElem);
-};
-
-function reset(elemID) {
-    let par = elemID.parentElement;
-    let inp = elemID;
-    console.log(elemID.value);
-
-    // par.removeChild(document.getElementById(pcb[0]));
-    // par.removeChild(document.getElementById(pcb[1]));
-    // par.appendChild(inp);
+        radioElem.value = false;
+        outputElem.innerText = inputElem.value;
+        inputElem.setAttribute("value", "");
+    } else {
+        data[`item${index}`]['editable'] = true;
+        inputElem.style.display = "block";
+        outputElem.style.display = "none";
+        radioElem.style.display = "none";
+    };
 };
