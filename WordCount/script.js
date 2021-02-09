ef8936b4423838af859e1c0388e989de274ecefa
Wraith <i.acnaylor@gmail.com>
2021-02-09 22:15:08 +0000

    Added wordcount page

---
+++ WordCount/script.js
@@ -0,0 +1,14 @@
+function count() {
+    let text = document.querySelector(".input-area").value;
+    let info = document.getElementById("results");
+
+    let words, chars;
+    
+    words = text.split(" ").length;
+    chars = text.length;
+
+    info.innerText = (`Words: ${words} ${<br/>}  Chars: ${chars}`);
+
+    console.log("Words: " + words);
+    console.log("Chars: " + chars);
+};
